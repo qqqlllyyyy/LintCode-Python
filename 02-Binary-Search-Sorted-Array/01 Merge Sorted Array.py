1eb653dc84a17776d7ac902bf7e19a5031b30fec
Liyu Qin <qinliyu1990@gmail.com>
2015-08-30 00:51:38 -0700

    Create 01 Merge Sorted Array.py

---
+++ 02-Binary-Search-Sorted-Array/01 Merge Sorted Array.py
@@ -0,0 +1,23 @@
+# Merge Sorted Array
+# Given two sorted integer arrays A and B, merge B into A as one sorted array.
+#
+# Example
+# A = [1, 2, 3, empty, empty], B = [4, 5]
+# After merge, A will be filled as [1, 2, 3, 4, 5]
+#
+# Note
+# You may assume that A has enough space (size that is greater or equal to m + n)
+# to hold additional elements from B. The number of elements initialized in A
+# and B are m and n respectively.
+
+class Solution:
+    """
+    @param A: sorted integer array A which has m elements, 
+              but size of A is m+n
+    @param B: sorted integer array B which has n elements
+    @return: void
+    """
+    def mergeSortedArray(self, A, m, B, n):
+        for i in range(n):
+            A[m + i] = B[i];
+        A.sort();
