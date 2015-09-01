bac7efbe9f7983b387bea909aff080d1986d1b5e
Liyu Qin <qinliyu1990@gmail.com>
2015-09-01 11:07:18 -0700

    Create 02 Insert Node in a Binary Search Tree.py

---
+++ 03-Binary-Tree-Divide-Conquer/02 Insert Node in a Binary Search Tree.py
@@ -0,0 +1,45 @@
+# Insert Node in a Binary Search Tree
+# Given a binary search tree and a new tree node, insert the node 
+# into the tree. You should keep the tree still be a valid binary 
+# search tree.
+#
+# Challenge
+# Can you do it without recursion?
+#
+
+
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+class Solution:
+    """
+    @param root: The root of the binary search tree.
+    @param node: insert this node into the binary search tree.
+    @return: The root of the new binary search tree.
+    """
+    def insertNode(self, root, node):
+        # write your code here
+        if root is None:
+            return node
+        
+        cur = root
+        while cur != node:
+            if node.val < cur.val:
+                if cur.left is not None:
+                    cur = cur.left
+                else:
+                    cur.left = node
+                
+            if node.val > cur.val:
+                if cur.right is not None:
+                    cur = cur.right
+                else:
+                    cur.right = node
+                
+        return root ## Can not return node, why ??
+        ## How to do it with recursion? 
