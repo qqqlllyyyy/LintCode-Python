ecb9b1df4092afb52f6a48cceeb92388bfe61ec6
Liyu Qin <qinliyu1990@gmail.com>
2015-09-02 19:50:31 -0700

    Create 02 Unique Paths II.py

---
+++ 04-Dynamic-Programming-I/02 Unique Paths II.py
@@ -0,0 +1,48 @@
+# Unique Paths II
+# Follow up for "Unique Paths":
+# Now consider if some obstacles are added to the 
+# grids. How many unique paths would there be?
+# An obstacle and empty space is marked as 1 and 0
+# respectively in the grid.
+#
+# For example,
+# There is one obstacle in the middle of a 3x3 grid as illustrated below.
+#
+# [
+#  [0,0,0],
+#  [0,1,0],
+#  [0,0,0]
+# ]
+# The total number of unique paths is 2.
+#
+#
+#
+#
+
+class Solution:
+    """
+    @param obstacleGrid: An list of lists of integers
+    @return: An integer
+    """
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        # write your code here
+        m = len(obstacleGrid); n = len(obstacleGrid[0])
+        res = [[0 for i in range(n)] for j in range(m)]
+        for i in range(m):
+            if obstacleGrid[i][0] == 0:
+                res[i][0] = 1
+            else:
+                res[i][0] == 0
+                break
+        for i in range(n):
+            if obstacleGrid[0][i] == 0:
+                res[0][i] = 1
+            else:
+                res[0][i] = 0
+                break
+        for i in range(1, m): # Remember to start from 1, not 0 !
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1: res[i][j] = 0
+                else:
+                    res[i][j] = res[i-1][j] + res[i][j-1]
+        return res[m-1][n-1]
