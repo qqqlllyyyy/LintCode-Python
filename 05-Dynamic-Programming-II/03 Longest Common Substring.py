9ae32eeffb48a54d8f098c5d52f0325b9a5c5147
Liyu Qin <qinliyu1990@gmail.com>
2015-09-07 20:08:14 -0700

    Update 03 Longest Common Substring.py

--- 05-Dynamic-Programming-II/03 Longest Common Substring.py
+++ 05-Dynamic-Programming-II/03 Longest Common Substring.py
@@ -18,6 +18,32 @@
 class Solution:
     # @param A, B: Two string.
     # @return: the length of the longest common substring.
+
+    # Dynamic Programming
+    
+    def longestCommonSubstring(self, A, B):
+        
+        m = len(A)
+        n = len(B)
+        dp = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = 0
+        
+        maxLength = 0
+        for i in range(m + 1):
+            for j in range(n + 1):
+                maxLength = max(maxLength, dp[i][j])
+        
+        return maxLength
+
+    # Bad Method
+    
     def longestCommonSubstring(self, A, B):
         
         maxLength = 0
