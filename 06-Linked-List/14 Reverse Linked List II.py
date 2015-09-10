2fcf625769919a127dab62469bb708c718355b00
Liyu Qin <qinliyu1990@gmail.com>
2015-09-10 14:09:00 -0700

    Update 14 Reverse Linked List II.py

--- 06-Linked-List/14 Reverse Linked List II.py
+++ 06-Linked-List/14 Reverse Linked List II.py
@@ -1,3 +1,10 @@
+# Reverse Linked List II
+# Reverse a linked list from position m to n.
+#
+# Example
+# Given 1->2->3->4->5->NULL, m = 2 and n = 4, return 1->4->3->2->5->NULL.
+# Challenge
+# Reverse it in-place and in one-pass
 """
 Definition of ListNode
 
