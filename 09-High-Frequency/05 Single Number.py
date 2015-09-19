ba970a30739b4c6c4c792eec2554916761fca03b
Liyu Qin <qinliyu1990@gmail.com>
2015-09-19 16:19:54 -0700

    Create 05 Single Number.py

---
+++ 09-High-Frequency/05 Single Number.py
@@ -0,0 +1,30 @@
+# Single Number
+# Given 2*n + 1 numbers, every numbers occurs twice except one, find it.
+#
+# Example
+# Given [1,2,2,1,3,4,3], return 4
+#
+# Challenge
+# One-pass, constant extra space.
+
+"""
+    Bitwise exclusive:
+    0^0=0, 0^1=1, 1^0=1, 1^1=0
+    Add a number twice from 0:
+    0 ^0-> 0 ^0 ->0
+    0 ^1-> 1 ^0 ->0
+    So the number left is the single number we want.
+"""
+
+class Solution:
+    """
+    @param A : an integer array
+    @return : a integer
+    """
+    def singleNumber(self, A):
+        # write your code here
+        result = 0
+        for i in range(len(A)):
+            result = result ^ A[i]
+        return result
+        
